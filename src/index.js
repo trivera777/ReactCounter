1db1cb43d1b1c427893f99c1b94b7ef427a3e134
Tifanny Rivera <trivera51580@gmail.com>
2022-02-26 20:45:37 -0700

    new repo

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'react-bootstrap'
+import Counter from './components/counter'
+
+ReactDOM.render(<Counter />, document.getElementById('root'))

278fcfb89e7cd0e4398ca56a4154a1c8f098d63d
Tifanny Rivera <trivera51580@gmail.com>
2022-02-28 20:45:59 -0700

    set count state, renders accordingly

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
-import { Badge, Button } from 'react-bootstrap';
+import { Badge, Button } from "react-bootstrap";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
   };
 
-  handleIncrement(){
-    console.log("increment clicked")
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 })
   }
-  
 
   render() {
     let classes = "";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += this.state.count === 0 ? "warning" : "primary";
 
     return (
       <div>
-        <Badge bg={classes} className="m-2">{this.formatCount()} </Badge>
-        <Button onClick={this.handleIncrement} variant="secondary" size="sm">Increment</Button>
+        <Badge bg={classes} className="m-2">
+          {this.formatCount()}{" "}
+        </Badge>
+        <Button onClick={this.handleIncrement} variant="secondary" size="sm">
+          Increment
+        </Button>
       </div>
     );
   }
 
-  formatCount(){
-      const { count } = this.state;
-      return count === 0 ? "zero" : count;
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
   }
 }
 

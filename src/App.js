8bcfb937aef03c5f30e8bb1e16c06eb263ffd6fd
Tifanny Rivera <trivera51580@gmail.com>
2022-06-30 19:40:30 -0600

    adding count container

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App(){
             <div className="card text-center my-5">
                 <div className="card-body">
                     <h1>Counter App</h1>
+                    <div className="my-5">
+                        <h2>Count</h2>
+                    </div>
                 </div>
             </div>
         </div>
